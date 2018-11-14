fe6bac1e5dd3c6402272709433cf5b2887a9241a
krisnaaa268 <41616624+krisnaaa268@users.noreply.github.com>
2018-11-14 21:53:17 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {createMaterialTopTabNavigator} from 'react-navigation';
+
+import {Home} from './Components/Home';
+import {Heroes} from './Components/Heroes';
+import {Setting} from './Components/Setting';
+
+const NavBar = createMaterialTopTabNavigator(
+  {
+    Home: {screen: Home,},
+    Heroes: {screen: Heroes},
+    Setting: {screen: Setting}
+  },
+  {
+    tabBarPosisition: 'bottom',
+    swipeEnabled: true,
+    tabBarOption:
+    {
+      avtiveTintColor: '#f8f8f8f8',
+      activeBackgroundColor: '#586598',
+      inactiveTintColor: '#1234',
+      style: {
+        backgroundColor:'#000055',
+      },
+
+      labelStyle: {
+        fontSize: 10,
+        color: 'white',
+        padding: 5,
+      }
+    }
+  }
+);
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavBar/>
+    );
+  }
+}
